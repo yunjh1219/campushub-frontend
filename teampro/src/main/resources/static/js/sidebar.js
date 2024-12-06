e7eb570c7bd79d7fb8654b1886700b88bdd1f021
yunjh1219 <orenjh1219@gmail.com>
2024-12-06 15:30:21 +0900

    default-layout

--- teampro/src/main/resources/static/js/sidebar.js
+++ teampro/src/main/resources/static/js/sidebar.js
@@ -7,12 +7,8 @@ $(document).ready(function(){
         // depth2 메뉴가 화면상에 보일 때는 위로 보드랍게 접고 아니면 아래로 보드랍게 펼치기
         if( depth2.is(":visible") ){
             depth2.slideUp();
-            // 화살표 회전 원래대로 되돌리기
-            $(this).parent().removeClass("open");
         }else{
             depth2.slideDown();
-            // 화살표 180도 회전
-            $(this).parent().addClass("open");
         }
     });
 });
@@ -24,14 +20,10 @@ $(".snb_depth2 li>a").click(function(){
     // depth3 메뉴가 화면상에 보일 때는 위로 보드랍게 접고 아니면 아래로 보드랍게 펼치기
     if( depth3.is(":visible") ){
         depth3.slideUp();
-        // 화살표 회전 원래대로 되돌리기
-        $(this).parent().removeClass("open");
     }else{
         depth3.slideDown();
-        // 화살표 180도 회전
-        $(this).parent().addClass("open");
-    }
 
+    }
     // 클릭 시 부모 메뉴인 depth2는 열리지 않도록
     return false;  // 이벤트 버블링을 막음
 });
