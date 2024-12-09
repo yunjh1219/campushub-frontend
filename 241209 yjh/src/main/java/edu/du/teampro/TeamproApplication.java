3a38e140dc6f816d498abdf04e1aab4563cba65a
yunjh1219 <orenjh1219@gmail.com>
2024-12-09 18:39:52 +0900

    jsy 합병

---
+++ 241209 yjh/src/main/java/edu/du/teampro/TeamproApplication.java
@@ -0,0 +1,13 @@
+package edu.du.teampro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamproApplication.class, args);
+    }
+
+}
